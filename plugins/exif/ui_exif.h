2e5d01b67ca5fc63dbd2daa65728c0148bcebcb2
falsecam <FalseCAM@googlemail.com>
2011-06-12 11:59:36 +0200

    CMakeLists.txt updates
    Signed-off-by: falsecam <FalseCAM@googlemail.com>

--- plugins/exif/ui_exif.h
+++ plugins/exif/ui_exif.h
@@ -1,7 +1,7 @@
 /********************************************************************************
 ** Form generated from reading UI file 'exif.ui'
 **
-** Created: Sat 11. Jun 21:57:09 2011
+** Created: Sun 12. Jun 11:51:26 2011
 **      by: Qt User Interface Compiler version 4.7.0
 **
 ** WARNING! All changes made in this file will be lost when recompiling UI file!
